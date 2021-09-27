3d998480b62441d73d35274e4aea9d72c6af1c93
Adam <adammoore118@outlook.com>
2021-09-27 12:50:40 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    tasker(document.getElementById('new-task-description').value);
+
+  })
 });
+
+function tasker(todo){
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  let li = document.createElement('li')
+  let ul = document.getElementById('tasks')
+  li.textContent = `  ${todo} `
+  ul.appendChild(li)
+  li.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
